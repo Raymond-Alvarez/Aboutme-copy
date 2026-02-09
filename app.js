f777187542ae87618dd965038a59152c00d78ed2
Naomi Torres <ivanys.ntm@gmail.com>
2026-02-09 12:01:39 -0800

    fixed app.js

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
 'use strict';
-let userPoints = 0;
 
+let user = ""; // declare user first
+let score = 0;
+
+// Welcome message
 alert("｡･:*:･ﾟ★｡ Welcome to the Guessing Game! ｡･:*:･ﾟ★｡");
 
+// Ask for user name and block "Naomi"
 while (!user || user.toLowerCase() === "naomi") {
   if (!user) {
-    let user = prompt("What is your name? I REALLY want to know...");
+    user = prompt("What is your name? I REALLY want to know...");
   } else if (user.toLowerCase() === "naomi") {
-    let user = prompt("Hmm...Nice Try!");
+    user = prompt("Hmm...Nice Try! Enter another name.");
   }
 }
 
-if (user.toLowerCase() === "naomi") {
-  alert("Hmm… nice try!");
-}
+// Greet the user
+alert("Hi " + user + "! I am so glad you decided to visit my site.");
 
-alert("Hi " + user +  "! I am so glad you decided to visit my site.");
-
-let score = 0;
-
-// reusable function for 3 attempts
+// Reusable function for questions with 3 attempts
 function askQuestion(question, correctAnswer) {
   let attempts = 0;
 
   while (attempts < 3) {
     let guess = prompt(question);
 
-    if (guess && guess.toLowerCase() === correctAnswer) {
+    if (guess && guess.toLowerCase() === correctAnswer.toLowerCase()) {
       alert("Correct! ᯓ★");
       return true;
     } else {
@@ -42,21 +41,15 @@ function askQuestion(question, correctAnswer) {
 }
 
 // Question 1
-if (askQuestion("Is Naomi a morning person or night owl?", "night owl")) {
-  score++;
-}
+if (askQuestion("Is Naomi a morning person or night owl?", "night owl")) score++;
 
 // Question 2
-if (askQuestion("Pineapple on pizza: yes or no?", "yes")) {
-  score++;
-}
+if (askQuestion("Pineapple on pizza: yes or no?", "yes")) score++;
 
 // Question 3
-if (askQuestion("Cats or dogs (or both)?", "cats")) {
-  score++;
-}
+if (askQuestion("Cats or dogs (or both)?", "cats")) score++;
 
-// Question 4 (number question with 3 tries)
+// Question 4 (number guessing)
 let numberAttempts = 0;
 while (numberAttempts < 3) {
   let q4 = prompt("Guess a number from 1–5");
@@ -67,28 +60,19 @@ while (numberAttempts < 3) {
     break;
   } else {
     numberAttempts++;
-    if (numberAttempts < 3) {
-      alert("Try again!");
-    } else {
-      alert("Nope! The correct number was 3.");
-    }
+    if (numberAttempts < 3) alert("Try again!");
+    else alert("Nope! The correct number was 3.");
   }
 }
 
 // Question 5
-if (askQuestion("Sweet or salty?", "salty")) {
-  score++;
-}
+if (askQuestion("Sweet or salty?", "salty")) score++;
 
 // Question 6
-if (askQuestion("Reading or audiobooks?", "reading")) {
-  score++;
-}
+if (askQuestion("Reading or audiobooks?", "reading")) score++;
 
 // Question 7
-if (askQuestion("Socks or barefoot?", "barefoot")) {
-  score++;
-}
+if (askQuestion("Socks or barefoot?", "barefoot")) score++;
 
 // Final score
 alert("Game over! ˙𐃷˙ You got " + score + " out of 7 correct!");
